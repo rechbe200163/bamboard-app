5c81c17cf45cde2ff5ab047d174bcbfbbcd096cb
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2025-08-13 21:19:47 +0200

    Refactor app structure: remove tab layout and explore screens, add HomeScreen component with introductory content

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import React from 'react';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -18,12 +23,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <React.Fragment>
+      <Stack />;
+      <StatusBar style='auto' />
+    </React.Fragment>
   );
 }
